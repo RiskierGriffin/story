0b1f7e18765460f00e97326b35710c99275ae33e
RiskierGriffin <tenenbaumallan@gmail.com>
2018-04-24 12:32:18 -0400

    did more work. need to read the form.

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 function initialize(){
     storyIndex = 0;
-    alert(STORIES[1]);
+    userWords = [];
     storySplitter();
+    formCreate();
+    
 }
 function storySplitter(){
     splitStory = STORIES[storyIndex].split(DELIMITER);
-    alert(splitStory[0]);
-    for(var i = 1;i < (STORIES[storyIndex].length); i=i+2){
-        alert(splitStory[i]);
+    alert(splitStory);
+    for(var i = 1;i < (splitStory.length); i= i+2){
+        userWords.push(splitStory[i]);
+        
     }
+    alert(splitStory);
+}
+function formCreate(){
+    for(var i = 0; i < userWords.length; i++){
+        var newdiv = document.createElement('form');
+        newdiv.innerHTML = userWords[i] + " <br><input type='text' name='userInput[]'>";
+        document.getElementById("form").appendChild(newdiv);
+    }
+}
+function replaceWords(){
+    alert(splitStory);
 }
